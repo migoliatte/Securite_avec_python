b2a50815eb4b0600c742ab881f3788f8f5930d94
Migoliatte <ramond.valentin99@gmail.com>
2021-05-12 20:33:40 +0200

    Ajout couleur

--- ping.py
+++ ping.py
@@ -5,11 +5,12 @@ import socket
 import ipaddress
 import time
 
+
 def help():
-    print("Function _ help")
     print("-h Affiche ce message")
-    print("-p Ping")
-    print("-sp Ping les ports via socket")
+    print("-p Ping incompatible avec -sp & -sd")
+    print("-sp Ping les ports via socket incompatible avec -p & -sd")
+    print("-sd Ping les sous domaines incompatible avec -sp & -p")
     print("-o N'affiche pas le retour dans le cli mais dans des fichiers")
 
 def ping(export,addr,fichier):
@@ -18,28 +19,33 @@ def ping(export,addr,fichier):
             if(export):
                 fichier.write("L'ip "+str(addr)+" a repondu\n")
             else:
-                print("L'ip "+str(addr)+" a repondu")
-        #elif(p.returncode == 1):
-        #    if(export):
-        #        fichier.write("L'ip "+str(addr)+" n'a pas repondu\n")
-        #    else:
-        #        print("L'ip "+str(addr)+" n'a pas  repondu\n")
-    
+                print("")
+                print('\033[2;32m'+str(addr)+' \033[0;0m')
+                #print("L'ip "+str(addr)+" a repondu")
+        else:
+            if(export):
+                fichier.write("L'ip "+str(addr)+" n'a pas repondu\n")
+            else:
+                #print("L'ip "+str(addr)+" n'a pas repondu\n")
+                print('\033[2;31m'+str(addr)+' \033[0;0m',end="")
+
 def socket_ping(export,addr,fichier,sock):
     ports=[20,21,22,23,25,53,80,110,111,135,139,143,443,445,993,995,1723,3306,3389,5900,8080]
-    #ports=[22,80]
     for i in ports:
-        if sock.connect_ex((str(addr),i)) == 0:
+        if sock.connect_ex((str(addr),i))  == 0:
             if(export):
                 fichier.write("Le port "+str(i)+" est ouvert sur l'ip "+(str(addr))+"\n")
             else:
-                print("Ping de "+(str(addr))+" sur le port "+str(i)+" en cours .... Time : "+str(time.localtime().tm_hour)+"heures "+str(time.localtime().tm_min)+"minutes "+str(time.localtime().tm_sec)+"secondes." )
-                print("Le port "+str(i)+" est ouvert sur l'ip "+(str(addr)))
-        #else:
-        #    if(export):
-        #        fichier.write("Le port "+str(i)+" n'est pas ouvert sur l'ip "+(str(addr))+"\n")
-        #    else:
-        #        print("Le port "+str(i)+" n'est pas ouvert sur l'ip "+(str(addr))+"\n")
+                #print("Ping de "+(str(addr))+" sur le port "+str(i)+" en cours .... Time : "+str(time.localtime().tm_hour)+"heures "+str(time.localtime().tm_min)+"minutes "+str(time.localtime().tm_sec)+"secondes." )
+                #print("Le port "+str(i)+" est ouvert sur l'ip "+(str(addr)))
+                print('\033[2;32m'+str(i)+' \033[0;0m', end='')
+
+        else:
+            if(export):
+                fichier.write("Le port "+str(i)+" n'est pas ouvert sur l'ip "+(str(addr))+"\n")
+            else:
+                #print("Le port "+str(i)+" n'est pas ouvert sur l'ip "+(str(addr))+"\n")
+                print('\033[2;31m'+str(i)+' \033[0;0m', end='')
 
 def sous_domaine():
     print("Function _ sous_domaine")
@@ -48,25 +54,27 @@ def ip_selection(array):
     network = input("Entrez votre réseau à ping avec le cidre : (par défaut 192.168.1.0/24) ") 
     if network == "":
         network="192.168.1.0/24"
-
-    if(array[2]):
+    fichier = ""
+    second_fichier = ""
+    if(array[2] and array[1]):
         fichier = open("ip_online.txt", "a+")
     if(array[3]):
-        second_fichier = open("port_open.txt", "a+")
+        if(array[1]):
+            second_fichier = open("port_open.txt", "a+")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
     for addr in ipaddress.IPv4Network(network).hosts():
-        #print("<---------------"+str(addr)+"--------------->")
         if(array[2]):
             ping(array[1],addr,fichier)
         if(array[3]):
+            print("<---------------"+str(addr)+"---------------> ",end="")
             socket_ping(array[1],addr,second_fichier,sock)
+            print("")
         
-
-    if(array[2]):
+    if(array[2] and array[1]):
         fichier.close()
-    if(array[3]):
+    if(array[3] and array[1]):
         second_fichier.close()
 
 def menu():
@@ -88,8 +96,12 @@ def main():
     array=menu()
     if array[0] == 1:
         help()
+
         exit()
-    elif array[2] == 1 | array[3] == 1:
+    elif array[2] == 1 and array[3] == 1 or array[2] == 1 and array[4] == 1 or array[4] == 1 and array[3] == 1:
+        help()
+        exit()
+    elif array[2] == 1 or array[3] == 1:
         ip_selection(array)
     elif array[4] == 1:
         sous_domaine()
