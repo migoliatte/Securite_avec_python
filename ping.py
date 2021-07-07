a5e3c867900ff539b461172f37c90619de9ee256
Migoliatte <46922406+migoliatte@users.noreply.github.com>
2021-07-07 10:36:53 +0200

    Update final avec domains

--- ping.py
+++ ping.py
@@ -22,17 +22,20 @@ def ping(export,addr,fichier):
             else:
                 print("")
                 print('\033[2;32m'+str(addr)+' \033[0;0m')
-                print("L'ip "+str(addr)+" a repondu")
+                #print("L'ip "+str(addr)+" a repondu")
         else:
             if(export):
                 fichier.write("L'ip "+str(addr)+" n'a pas repondu\n")
-            else:
-                print("L'ip "+str(addr)+" n'a pas repondu\n")
-                print('\033[2;31m'+str(addr)+' \033[0;0m',end="")
+            #else:
+                #print("L'ip "+str(addr)+" n'a pas repondu\n")
+                #print('\033[2;31m'+str(addr)+' \033[0;0m',end="")
 
-def socket_ping(export,addr,fichier,sock):
-    ports=[20,21,22,23,25,53,80,110,111,135,139,143,443,445,993,995,1723,3306,3389,5900,8080]
+def socket_ping(export,addr,fichier):
+    #ports=[20,21,22,23,25,53,80,110,111,135,139,143,443,445,993,995,1723,3306,3389,5900,8080]
+    ports=[22,2910]
     for i in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(.2)
         if sock.connect_ex((str(addr),i))  == 0:
             if(export):
                 fichier.write("Le port "+str(i)+" est ouvert sur l'ip "+(str(addr))+"\n")
@@ -41,20 +44,43 @@ def socket_ping(export,addr,fichier,sock):
                 #print("Le port "+str(i)+" est ouvert sur l'ip "+(str(addr)))
                 print('\033[2;32m'+str(i)+' \033[0;0m', end='')
 
+        #else:
+        #    if(export):
+        #        fichier.write("Le port "+str(i)+" n'est pas ouvert sur l'ip "+(str(addr))+"\n")
+        #    else:
+        #        #print("Le port "+str(i)+" n'est pas ouvert sur l'ip "+(str(addr))+"\n")
+        #        print('\033[2;31m'+str(i)+' \033[0;0m', end='')
+        sock.close()
+
+def sous_domaine(export):
+    site = input("Entrez le domaine du site : (par défaut google.fr) ") 
+    if site == "":
+        site="google.fr"
+    fichier = ""
+    print("Test des sous domaines de "+site)
+    s_dmn=["www","fr","software","test"]
+    for i in s_dmn:
+        p = subprocess.run('ping -n 1 -w 1 '+str(i)+"."+str(site),stdout=subprocess.PIPE,shell=True)
+        if (p.returncode == 0):
+            if(export):
+                fichier = open("sous_domaine.txt", "a+")
+                fichier.write("L'ip "+str(s_dmn)+" a repondu\n")
+                fichier.close()
+            else:
+                print('\033[2;32m'+str(i)+' \033[0;0m',end="")
         else:
             if(export):
-                fichier.write("Le port "+str(i)+" n'est pas ouvert sur l'ip "+(str(addr))+"\n")
-            else:
-                #print("Le port "+str(i)+" n'est pas ouvert sur l'ip "+(str(addr))+"\n")
-                print('\033[2;31m'+str(i)+' \033[0;0m', end='')
+                fichier = open("sous_domaine.txt", "a+")
+                fichier.write("L'ip "+str(s_dmn)+" n'a pas repondu\n")
+                fichier.close()
 
-def sous_domaine():
-    print("Function _ sous_domaine")
+            else:
+                print('\033[2;31m'+str(i)+' \033[0;0m',end="")
 
 def ip_selection(array):
     network = input("Entrez votre réseau à ping avec le cidre : (par défaut 192.168.1.0/24) ") 
     if network == "":
-        network="192.168.157.0/24"
+        network="192.168.1.0/24"
     fichier = ""
     second_fichier = ""
     if(array[2] and array[1]):
@@ -62,17 +88,15 @@ def ip_selection(array):
     if(array[3]):
         if(array[1]):
             second_fichier = open("port_open.txt", "a+")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.1)
-
-
+        #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #sock.settimeout(10)
 
     for addr in ipaddress.IPv4Network(network).hosts():
         if(array[2]):
             ping(array[1],addr,fichier)
         if(array[3]):
             print("<---------------"+str(addr)+"---------------> ",end="")
-            socket_ping(array[1],addr,second_fichier,sock)
+            socket_ping(array[1],addr,second_fichier)
             print("")
         
     if(array[2] and array[1]):
@@ -104,7 +128,7 @@ def main():
     elif array[2] == 1 or array[3] == 1:
         ip_selection(array)
     elif array[4] == 1:
-        sous_domaine()
+        sous_domaine(array[1])
     else:
         help()
 
